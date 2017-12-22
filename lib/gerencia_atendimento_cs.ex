5fd00a8e89b21661088d055e1a6edf4a14b0a6cf
unknown <diegoleonardo@outlook.com>
2017-12-22 13:14:04 -0200

    Resolução do problema de cs

--- lib/gerencia_atendimento_cs.ex
+++ lib/gerencia_atendimento_cs.ex
@@ -6,7 +6,25 @@ defmodule GerenciaAtendimentoCs do
 
   def ordena_clientes_para_atendimento(lista_cs, lista_clientes) when is_list(lista_cs) and is_list(lista_clientes) do
     lista_cs_ordenada = Enum.sort(lista_cs, &(&1.nivel_atendimento <= &2.nivel_atendimento)) 
-    Enum.map(lista_cs_ordenada, fn x -> selecionar_clientes_para_atendimento(x, lista_clientes) end)
+    
+    cs_que_atende_menor_nivel = Enum.min_by(lista_cs, fn x -> x.nivel_atendimento end)
+
+    ordena_clientes_para_atendimento(lista_cs_ordenada, lista_clientes, cs_que_atende_menor_nivel)
   end
 
+  defp ordena_clientes_para_atendimento(lista_cs, lista_clientes, cs_que_atende_menor_nivel) when is_list(lista_cs) and is_list(lista_clientes) do
+    Enum.map(lista_cs, fn x ->
+                          cond do
+                            cs_que_atende_menor_nivel.nivel_atendimento == x.nivel_atendimento -> 
+                                selecionar_clientes_para_atendimento(x, lista_clientes)
+
+                            true -> 
+                                customer_success_anterior = Enum.filter(lista_cs, fn z -> z.nivel_atendimento < x.nivel_atendimento end) 
+                                                              |> List.last()
+
+                                selecionar_clientes_para_atendimento(x, lista_clientes)
+                                    |> Enum.filter(fn y -> y.nivel > customer_success_anterior.nivel_atendimento end)
+                          end
+    end)
+  end
 end
--- test/costumer_success_problem_test.exs
+++ test/costumer_success_problem_test.exs
@@ -8,19 +8,16 @@ defmodule CostumerSuccessProblemTest do
   setup [:cria_costumer_success, :cria_costumer_success_clientes]
 
   test "Deve preencher uma lista com 5 costumer success", %{lista_cs: lista_cs} do
-
     assert lista_cs != nil
     assert Enum.count(lista_cs) == 5
   end
 
   test "Deve preencher uma lista com 10 clientes que são atendidos pelos costumer success", %{lista_clientes_cs: lista_clientes_cs} do
-
     assert lista_clientes_cs != nil
     assert Enum.count(lista_clientes_cs) == 10
   end
   
   test "Costumer success deve atender apenas clientes menor ou igual ao seu nivel", %{lista_clientes_cs: lista_clientes_cs}  do
-    
     cs_nivel_20 = %CostumerSuccess{id: 1, descricao: "CS 1", nivel_atendimento: 20}
     
     clientes_atendidos = GerenciaAtendimentoCs.selecionar_clientes_para_atendimento(cs_nivel_20, lista_clientes_cs)
@@ -44,6 +41,50 @@ defmodule CostumerSuccessProblemTest do
                         [%CostumerSuccessClient{id: 8, nivel: 90}, %CostumerSuccessClient{id: 10, nivel: 100}]
                       ]
     assert lista_ordenada == expected_result
+  end
+
+  test "Deve mostrar que o CS de nível de atendimento 60 também atende os clientes do CS de nível 40 quando o mesmo estiver ausente", %{lista_cs: lista_cs, lista_clientes_cs: lista_clientes_cs} do
+    lista_cs_sem_nivel_atendimento_40 = Enum.reject(lista_cs, fn x -> x.nivel_atendimento == 40 end) 
+    lista_clientes = GerenciaAtendimentoCs.ordena_clientes_para_atendimento(lista_cs_sem_nivel_atendimento_40, lista_clientes_cs)
+    
+    expected_result = [
+      [%CostumerSuccessClient{id: 1, nivel: 10}, %CostumerSuccessClient{id: 2, nivel: 20}],  
+      [%CostumerSuccessClient{id: 3, nivel: 30}, %CostumerSuccessClient{id: 4, nivel: 40},
+        %CostumerSuccessClient{id: 5, nivel: 50}, %CostumerSuccessClient{id: 7, nivel: 60}], 
+      [%CostumerSuccessClient{id: 6, nivel: 70}, %CostumerSuccessClient{id: 9, nivel: 80}], 
+      [%CostumerSuccessClient{id: 8, nivel: 90}, %CostumerSuccessClient{id: 10, nivel: 100}]
+    ]
+
+    assert expected_result == lista_clientes
+  end
+
+  test "Deve mostrar uma lista com os clientes só até o nível 40 quando tiver customer success com nível de atendimento 20 e 40", %{lista_cs: lista_cs, lista_clientes_cs: lista_clientes_cs} do
+    lista_cs_com_nivel_atendimento_menor_igual_40 = Enum.reject(lista_cs, fn x -> x.nivel_atendimento > 40 end)
+
+    lista_clientes = GerenciaAtendimentoCs.ordena_clientes_para_atendimento(lista_cs_com_nivel_atendimento_menor_igual_40, lista_clientes_cs)
+
+    expected_result = [
+      [%CostumerSuccessClient{id: 1, nivel: 10}, %CostumerSuccessClient{id: 2, nivel: 20}],  
+      [%CostumerSuccessClient{id: 3, nivel: 30}, %CostumerSuccessClient{id: 4, nivel: 40}]
+    ]
+
+    assert expected_result == lista_clientes
+  end
+
+  test "Deve mostrar lista com todos os clientes quando tiver só o customer success com nível de atendimento 100", %{lista_cs: lista_cs, lista_clientes_cs: lista_clientes_cs} do
+      lista_cs_somente_com_nivel_atendimento_100 = Enum.reject(lista_cs, fn x -> x.nivel_atendimento < 100 end)
+      
+      lista_clientes = GerenciaAtendimentoCs.ordena_clientes_para_atendimento(lista_cs_somente_com_nivel_atendimento_100, lista_clientes_cs)
+      
+      expected_result = [
+                         [%CostumerSuccessClient{id: 1, nivel: 10}, %CostumerSuccessClient{id: 2, nivel: 20},  
+                          %CostumerSuccessClient{id: 3, nivel: 30}, %CostumerSuccessClient{id: 4, nivel: 40},
+                          %CostumerSuccessClient{id: 5, nivel: 50}, %CostumerSuccessClient{id: 7, nivel: 60}, 
+                          %CostumerSuccessClient{id: 6, nivel: 70}, %CostumerSuccessClient{id: 9, nivel: 80}, 
+                          %CostumerSuccessClient{id: 8, nivel: 90}, %CostumerSuccessClient{id: 10, nivel: 100}]
+                      ]
+      
+      assert expected_result == lista_clientes
   end
 
   defp cria_costumer_success(_) do
